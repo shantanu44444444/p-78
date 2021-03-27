e6043849b6d72bf507756b9387acc7157ff742c8
shantanu44444444 <76909672+shantanu44444444@users.noreply.github.com>
2021-03-27 15:25:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,15 +3,27 @@ var images = [
     "vas.jfif",
     "shan.jfif"
 ];
+
+
 var names = [
     "Madhura Tikekar",
     "Vaishnavi Tikekar",
     "Shantanu Tikekar"
 ];
-var i = 0;
 
-function nextslide() {
-    document.getElementById("Album").src = images[i];
-    document.getElementById("lam").innerHTML = reasons[i];
-    i++;
-}
+
+
+var i = 0;
+            function nextslide()
+            {
+                i++;
+                var numbers_of_family_member_in_array = 3
+                if(i > numbers_of_family_member_in_array )
+                {
+                    i = 0;
+                }
+                var updatedImage = images[i];
+                var updatedName  = names[i];
+                document.getElementById("Album").src = updatedImage;
+                document.getElementById("lam").innerHTML = updatedName;
+            }
